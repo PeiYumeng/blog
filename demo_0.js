ac1d8623afc46379744fb53bc11bb4eb4d544538
PeiYumeng <457492875@qq.com>
2019-10-19 10:13:59 +0800

    message

--- demo_0.js
+++ demo_0.js
@@ -2,21 +2,20 @@ const fs = require('fs');
 const http = require('http');
 
 var server = http.createServer();
-server.on('request',function(req,res){
-    console.log('request'+JSON.stringify(req.headers));  //输出请求头
-    req.pipe(process.stdout);
-    req.end('hello world');
-});
+// server.on('request',function(req,res){
+//     console.log('request'+JSON.stringify(req.headers));  //输出请求头
+//     req.pipe(process.stdout);
+//     req.end('hello world');
+// });
 server.listen(8080,function(){
     console.log('listening on %d',this.address().port)
 });
-
-server.on('request',function(request,response){
-		
-    var url = request.url;
-    if(url ==='/'){
+server.
+server.on('request',function(req,res){
+    var url = req.url;
+    if(url ==='/login'){
         //response.writeHead(响应状态码，响应头对象): 发送一个响应头给请求。
-        response.writeHead(200,{'Content-Type':'text/html'});
+        res.writeHead(200,{'Content-Type':'text/html'});
         // 如果url=‘/’ ,读取指定文件下的html文件，渲染到页面。
         fs.readFile('./login.html','utf-8',function(err,data){
             if(err){
